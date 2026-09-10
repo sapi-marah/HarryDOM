6eb22ea521c3db88f1cc1c5fa6c4228e0eadc280
sapi-marah <Email-7acd024b3df24e40@teamchat.zoom.us>
2026-09-10 14:46:47 -0700

    Implement token count validation
    Add a check for token count to handle low token scenarios.

--- prototypes/blooketTokenWaster.js
+++ prototypes/blooketTokenWaster.js
@@ -5,6 +5,12 @@ if (window.location.href === "https://dashboard.blooket.com/market") {
   for (let digit of par) {
     tokens = tokens + digit;
   }
+  tokens = Number(tokens);
+  if (tokens < 25) {
+    //do that maybe in 3217387317 years.
+  } else {
+    
+  }
 } else {
   alert("Invaild Page");
 }
