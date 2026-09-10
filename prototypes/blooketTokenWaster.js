1dbae19acd390071da45add4303321817d672a0d
sapi-marah <Email-7acd024b3df24e40@teamchat.zoom.us>
2026-09-10 14:44:57 -0700

    Update blooketTokenWaster.js

--- prototypes/blooketTokenWaster.js
+++ prototypes/blooketTokenWaster.js
@@ -1,7 +1,10 @@
 if (window.location.href === "https://dashboard.blooket.com/market") {
   let fetchedTokens = document.getElementsByClassName('TokenCounter_tokenContainer__wE6XL')[0].textContent;
-  let tokens = undefined;
-  fetched
+  let tokens = "";
+  var par = fetchedTokens.split(',');
+  for (let digit of par) {
+    tokens = tokens + digit;
+  }
 } else {
   alert("Invaild Page");
 }
